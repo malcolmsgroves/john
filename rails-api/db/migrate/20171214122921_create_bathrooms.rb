05da87104751234ff1475ce121bc0a4da3b27f0f
Malcolm Groves <macgroves@gmail.com>
2017-12-17 22:02:01 -0500

    Added migration for postgis extension

--- rails-api/db/migrate/20171214122921_create_bathrooms.rb
+++ rails-api/db/migrate/20171214122921_create_bathrooms.rb
@@ -1,5 +1,6 @@
 class CreateBathrooms < ActiveRecord::Migration[5.1]
   def change
+    enable_extension "postgis"
     create_table :bathrooms do |t|
       t.string :name
       t.st_point :lnglat
